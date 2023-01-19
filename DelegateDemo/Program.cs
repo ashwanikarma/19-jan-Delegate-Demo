10770d5230130c396f087c7261d6a0952167a6fe
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-19 08:45:15 +0530

    [Ashwani] - Added day 19 programs which was done in class

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using static DelegateDemo.Demo;
+
+namespace DelegateDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate Demo");
+            operation obj = new operation(Demo.Addition);
+            Console.WriteLine("Addition is = {0}", obj(23, 27));
+            //MulticastDelegate.ImplementDelegate();
+            Console.ReadKey();
+        }
+    }
+}
